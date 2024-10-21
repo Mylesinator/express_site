dd0e1ab3ac77679ac6f5a871c5cfcf1c87125b44
LazyKotaa <mylesmarshall06@gmail.com>
2024-10-21 00:51:41 -0700

    navbar & logo

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.sendFile("pages/index.html", { root: clientPath });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
